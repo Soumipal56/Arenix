e9f8c85ecd598a5c42e8df220e0a6fe93c3a01a8
Soumipal56 <soumiisc2020@gmail.com>
2026-05-13 13:16:54 +0530

    feat: initialize backend project with Express and LangGraph AI orchestration service

--- Backend/src/services/graph.ai.service.ts
+++ Backend/src/services/graph.ai.service.ts
@@ -1,25 +1,62 @@
-import { StateSchema, MessagesValue, StateGraph, START, END } from "@langchain/langgraph" 
+import { StateSchema, MessagesValue, ReducedValue, StateGraph, START, END } from "@langchain/langgraph" 
+import type { GraphNode } from "@langchain/langgraph"
+import { HumanMessage } from "@langchain/core/messages";
+import { mistralModel, cohereModel} from "./models.service.js"
+import { z } from "zod";
 
-type JUDGEMENT = {
-    winner: "solution_1" | "solution_2";
-    solution_1_score: number,
-    solution_2_score: number,
-}
 
-type AIBATTLESTATE = {
-    messages: typeof MessagesValue,
-    solution_1: String,
-    solution_2: String,
-    judgement: JUDGEMENT,
-}
-
-const state: AIBATTLESTATE = {
+const State = new StateSchema({
     messages: MessagesValue,
-    solution_1: "",
-    solution_2: "",
-    judgement: {
-        winner: "solution_1",
-        solution_1_score: 0,
-        solution_2_score: 0,
-    },
+    solution_1: new ReducedValue(z.string().default(""), {
+        reducer: (current, next) => {
+            return next
+        }
+    }),
+    solution_2: new ReducedValue(z.string().default(""), {
+        reducer: (current, next) => {
+            return next
+        }
+    }),
+    judge_recommendation: new ReducedValue(z.object().default({
+       solution_1_score: 0,
+       solution_2_score: 0,
+    }),
+    {
+        reducer: (current, next) => {
+            return next
+        }
+    }
+)});
+
+const solutionNode:GraphNode<typeof State> = async (state:typeof State)=>{
+
+    console.log("Current State:", state)
+
+    const [mistral_solution, cohere_solution] = await Promise.all([
+        mistralModel.invoke(state.messages[0].content),
+        cohereModel.invoke(state.messages[0].content)
+    ])
+
+    return {
+        solution_1: mistral_solution.content as string,
+        solution_2: cohere_solution.content as string
+    }
 }
+
+const graph = new StateGraph(State)
+    .addNode("solution", solutionNode)
+    .addEdge(START, "solution")
+    .addEdge("solution", END)
+    .compile();
+
+export default async function(userMessage:string){
+    const result = await graph.invoke({
+        messages: [
+            new HumanMessage(userMessage)
+        ]
+    })
+
+    console.log(result)
+
+    return result.messages
+}  
